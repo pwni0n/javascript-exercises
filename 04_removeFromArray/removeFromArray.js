ac0046216d9bf6a2da3b8f86f17c09aef334e551
pwnion <110813874+pwni0n@users.noreply.github.com>
2023-11-05 05:52:01 -0600

    Update removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+    for (const element of elements) {
+        if (array.indexOf(element) !== -1) {
+            array.splice(array.indexOf(element), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
