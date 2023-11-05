016251bbe3b9ff70a66de17442deab46c7bf3d29
pwnion <110813874+pwni0n@users.noreply.github.com>
2023-11-05 05:17:43 -0600

    Update reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    const reversedText = [];
+    for (const letter of text) {
+        reversedText.unshift(letter);
+    }
+    return reversedText.join("");
 };
 
 // Do not edit below this line
