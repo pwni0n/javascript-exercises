8cb670d9b8c26c086e3e2dd53daca01ce9298157
pwnion <110813874+pwni0n@users.noreply.github.com>
2023-11-09 08:26:02 -0600

    Update findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.sort((a, b) => {
+        let yearA = a.yearOfDeath || new Date().getFullYear();
+        let yearB = b.yearOfDeath || new Date().getFullYear();
+        return (yearB - b.yearOfBirth) - (yearA - a.yearOfBirth);
+    });
+    return people[0];
 };
 
 // Do not edit below this line
