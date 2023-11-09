8c5863e116bb822211fad39389de1f4151c60ba2
pwnion <110813874+pwni0n@users.noreply.github.com>
2023-11-09 07:02:25 -0600

    Update palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let letters = string.toLowerCase().split("");
 
+    for (let i = 0; i < letters.length; i++) {
+        if (!!letters[i].match(/[.,:!? ]/)) {
+            letters.splice(i, 1);
+            --i;
+        }
+    }
+    
+    let normalWord = letters.join("");
+    let reversedWord = letters.reverse().join("");
+    return normalWord === reversedWord;
 };
 
 // Do not edit below this line
