376ad8b5b36040177ddb85bd08976d20f2addeb4
pwnion <110813874+pwni0n@users.noreply.github.com>
2023-11-09 07:10:28 -0600

    Update fibonacci function
    Uses recursion oooooo

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (+num < 3 && +num > 0) {
+        return 1;
+    } else if (num < 0) {
+        return "OOPS";
+    }
+    return fibonacci(+num - 1) + fibonacci(+num - 2);
 };
 
 // Do not edit below this line
